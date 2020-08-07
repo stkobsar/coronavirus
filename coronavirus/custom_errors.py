9fa27485caa1afefcc55477babfd121f6f48981f
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-07 19:01:05 +0200

    Handle wrong country entries and made recommender to sort it out

---
+++ coronavirus/custom_errors.py
@@ -0,0 +1,28 @@
+from builtins import Exception
+from difflib import SequenceMatcher
+
+
+class EmptyDataFrame(Exception):
+    pass
+
+def similar_name_country(country, paises):
+    #country es uno de los paises intriducidos por el usuario
+    #paises es todos los paises
+
+    prob_similar = []
+    for pais in paises:
+        prob_similar_value = similar(pais, country)
+        prob_similar.append(prob_similar_value)
+
+    index_max = prob_similar.index(max(prob_similar))
+    country_similar = paises[index_max]
+
+    return country_similar
+
+
+
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
--- coronavirus/plot_country.py
+++ coronavirus/plot_country.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import coronavirus.custom_errors as ce
 
 def pl_country_cases(country, csv, field="total_cases", savefig=True):
     df_coronavirus = pd.read_csv(csv)
+    paises = df_coronavirus["location"].unique()
+
+
     condition = df_coronavirus["location"].str.lower() == country.lower() #str.lower() retrieve en minuscula. Esto permite que le usuario pase spain como quiera
     df_filtered = df_coronavirus[condition]
+
+    if df_filtered.empty:
+        similar_country = ce.similar_name_country(country, paises)
+        raise ce.EmptyDataFrame(f'The name of country is not in the list of countries. Try to one of these {paises}. Did you mean {similar_country}?')
+
     df_filtered_na = df_filtered.fillna(0)
     list_cases = df_filtered_na[field].values
     list_date = range(len(list_cases))
