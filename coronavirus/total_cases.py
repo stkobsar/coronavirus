920389f9e1ee0da54484f433d372a320fe61cc96
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-12 19:59:39 +0200

    Automation of labeling in plots using the arguments of the function

--- coronavirus/total_cases.py
+++ coronavirus/total_cases.py
@@ -3,12 +3,27 @@ import coronavirus.plot_country as pl
 import matplotlib.pyplot as plt
 
 
-def pl_total_cases(csv, countries_user):
+def pl_total_cases(csv, countries_user, field):
     for country in countries_user:
-        plot_output = pl.pl_country_cases(country, csv, savefig=False)
+        plot_output = pl.pl_country_cases(country, csv, field, savefig=False)
+
     plt.legend(countries_user)
-    output = "cases_date_{}.png".format("_".join(countries_user))
-    plt.xlabel("Days since covid started")
-    plt.ylabel("COVID official cases")
-    plt.title("Analysis of coronavirus data")
+
+
+    plt.legend(countries_user)
+    curr_date = str(pd.to_datetime('today').date())
+    curr_date_custom = curr_date.replace('-', '')
+    field_custom = field.replace('_', '')
+    field_title = field.replace('_', ' ')
+    countries_user_outputname = "_".join(countries_user)
+    countries_plot_title = " ".join(countries_user)
+
+    plt.xlabel("Days since COVID-19 start spreading")
+    plt.ylabel(f"COVID-19 official cases of {field_title}")
+    plt.title(f"COVID-19 data of {field_title} in {countries_plot_title}")
+
+
+    output = f"{field_custom}_{curr_date_custom}_{countries_user_outputname}.png"
+
+
     plt.savefig(output)
