f46ee0d2ba5358806d9ccb062c42b1360699733e
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-31 01:35:04 +0200

    relative to population functionality

--- coronavirus/functionalities/relative_to_population.py
+++ coronavirus/functionalities/relative_to_population.py
@@ -1,8 +1,17 @@
-
+import pandas as pd
 
 def relative_population(absolute_values):
     CSV_POPULATION = "https://covid.ourworldindata.org/data/ecdc/locations.csv"
-    print(CSV_POPULATION)
+
+    population_df = pd.read_csv(CSV_POPULATION)
+    population_list = population_df["population"].unique()
+     ### Dividir valores de las listas entre si y meterlo en una nueva lista, que será list_cases para plotar.
+
     #CSV_POPULATION --> population
     #relative_values = num_cases / population
     #return realtive_values
+
+
+CSV_POPULATION = "https://covid.ourworldindata.org/data/ecdc/locations.csv"
+
+relative_population(CSV_POPULATION)
