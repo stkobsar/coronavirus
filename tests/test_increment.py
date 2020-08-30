9bc6db93a7438add79ebab742aa3aeef4999b31d
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-31 00:19:33 +0200

    get test data

--- tests/test_increment.py
+++ tests/test_increment.py
@@ -7,20 +7,17 @@ import coronavirus.functionalities.increment_days as id
 absolute_path_every_machine = os.path.abspath(__file__)
 directory_path = os.path.dirname(absolute_path_every_machine)
 
+
 ID = "full_data.csv"
-CSV = "https://covid.ourworldindata.org/data/ecdc/{0}".format(ID)
-
-def get_csv_test(CSV):
-    country = "Spain"
-    field = "total_cases"
-    csv = pc.dataset_clean(country, field, CSV)
-    tail_csv = csv.tail(30)
-    return tail_csv
-
-tail_csv = get_csv_test(CSV)
+csv = "https://covid.ourworldindata.org/data/ecdc/{0}".format(ID)
+df_coronavirus = pd.read_csv(csv)
+condition = df_coronavirus["location"] == "Spain"
+df_cond = df_coronavirus[condition]
+tail_csv = df_cond.tail(30)
 
 def increment_test(tail_csv):
     field = "total_cases"
+
     incremental = id.pl_increment_cases(tail_csv, field)
 
 
